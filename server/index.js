f8f8ef99854889f53a2d69af2fd761d8e1409755
Yogesh Sharma <yogeshs@dewsolutions.in>
2023-02-01 22:09:53 +0530

    Backend complete

--- server/index.js
+++ server/index.js
@@ -1,5 +1,32 @@
 const express = require("express");
 const app = express();
+const UserModel = require("./Models/users")
+const mongoose = require('mongoose');
+const cors = require ('cors');
+
+app.use(cors());
+app.use(express.json());
+mongoose.connect('mongodb+srv://Mern-Project-User:Mern-Project-User@clustermernproject.cat4msf.mongodb.net/mernProject1?retryWrites=true&w=majority')
+
+console.log(mongoose.connection.readyState);
+app.get(("/getUsers") , (req,res)=>{
+
+    UserModel.find({}, (err, result)=>{
+        if(err){
+            res.json(err)
+        }else {
+            res.json(result)
+        }
+    })
+});
+app.post("/createUser", async (req, res) => {
+    const user = req.body;
+    const newUser = new UserModel(user);
+    await newUser.save();
+
+    res.json(user);
+});
+
 
 app.listen(3001, ()=> {
     console.log ("Server is Working")
