f4334c49e60f99b4b72e644a0495d72f3f67e740
Yogesh Sharma <yogeshs@dewsolutions.in>
2023-02-02 13:53:11 +0530

    Project completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,52 @@
 import './App.css';
-
-import { useState } from 'react';
+import Axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [listOfUsers, setListOfUsers] = useState([{id: 1, name: "Yogesh", age: 37, username: "yogesh56"}]);
+  const [listOfUsers, setListOfUsers] = useState([]);
+  const [name, setName] = useState("")
+  const [age, setAge] = useState(0)
+  const [username, setUsername] = useState("")
+
+  useEffect(()=>{
+    Axios.get("http://localhost:3001/getUsers").then((response) =>{
+      setListOfUsers(response.data)
+    }
+
+    )
+  }, []);
+
+  const createUser = () => {
+    Axios.post("http://localhost:3001/createUser" ,{
+      name,
+      age,
+      username
+    }).then ((response) => {
+      
+      setListOfUsers([...listOfUsers, {name, age, username}]);
+      alert("User got Created");
+    });
+  };
+
   return (
     <div className="App">
       <div className='UsersDisplay'>
         {listOfUsers.map((user) =>{
           return(
             <div>
-              <h1>Name: {user.name}</h1>
-              <h2>Age: {user.age}</h2>
-              <h2>Username: {user.username}</h2>
+              <h2>Name: {user.name}</h2>
+              <h3>Age: {user.age}</h3>
+              <h3>Username: {user.username}</h3>
             </div>
           );
         })};
       </div>
-        
+      <div>
+        <input type='text' placeholder='Name' onChange={(event) => {setName(event.target.value)}}/>
+        <input type='number' placeholder='Age ' onChange={(event) => {setAge(event.target.value)}}/>
+        <input type='text' placeholder='username ' onChange={(event) => {setUsername(event.target.value)}}/>
+        <button onClick={createUser}> Create User</button>
+      </div>
     </div>
       
     
